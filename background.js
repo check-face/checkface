e8ec69f9a9599feabcb0e9df1e498cb6400f5819
apcteo <anapcteo@gmail.com>
2019-05-04 14:24:37 +1000

    Initial set-up of extension with context menu

---
+++ background.js
@@ -0,0 +1,25 @@
+// background.js
+var contextMenuItem={
+  "id": "checkFace",
+  "title": "Check Face",
+  "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(contextMenuItem);
+// Called when the user clicks on the browser action.
+chrome.contextMenus.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+});
+
+// This block is new!
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "open_new_tab" ) {
+      chrome.tabs.create({"url": request.url});
+    }
+  }
+);
