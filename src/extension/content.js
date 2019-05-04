2164fcf2ea4543315b9a0894445837e39830138f
Oliver Coad <oliver.coad@gmail.com>
2019-05-05 04:57:51 +1000

    Add commit SHA tooltips

--- src/extension/content.js
+++ src/extension/content.js
@@ -10,7 +10,7 @@ function showPopup(hashMessage) {
       <div id="FaceCheck">
         <div class="cfcontainer">
           <div class="checkface">
-            <p>sasfdfshdllfdaflafdsja</p>
+            <p id="checkfacehash">sasfdfshdllfdaflafdsja</p>
             <img id="checkfaceimg" width=300 height=300 />
           </div>
         </div>      
@@ -19,7 +19,9 @@ function showPopup(hashMessage) {
 
     document.body.appendChild(div);
     div.onclick = removeFunc;
-    document.querySelector("#checkfaceimg").src = "http://checkface.ml/api/" + hashMessage + "?dim=300";
+    div.querySelector("#checkfaceimg").src = "http://checkface.ml/api/" + hashMessage + "?dim=300";
+    div.querySelector("#checkfacehash").innerHTML = hashMessage;
+
   }
 
   function removeFunc() {
@@ -29,3 +31,60 @@ function showPopup(hashMessage) {
   $(document).ready(myfunc);
 }
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => showPopup(request.message));
+
+let commitSHA_Regex = /commit\/([a-zA-Z0-9]{40,40})($|\?|#)/;
+function addHoverFaceCheck(node) {
+  if(node.querySelector("#cfToolTip")) {
+    return;
+  }
+  let res = commitSHA_Regex.exec(node.href)
+  if(res) {
+    hashMessage = res[1]
+    // node.style.color = 'yellow';
+    var div = document.createElement('div');
+    div.id = "cfToolTip";
+    div.innerHTML = `
+    <div class="cfcontainer">
+      <div class="checkface">
+        <p id="checkfacehash">sasfdfshdllfdaflafdsja</p>
+        <img id="checkfaceimg" width=200 height=200 />
+      </div>
+    </div>
+    `
+    div.querySelector("#checkfaceimg").src = "http://checkface.ml/api/" + hashMessage + "?dim=200";
+    div.querySelector("#checkfacehash").innerHTML = hashMessage.substring(0, 7);
+    node.appendChild(div);
+  }
+}
+
+function observeAllLinks() {
+  let selector = 'a[href*=commit]';
+  // Initialize results with current nodes.
+  var result = Array.prototype.slice.call(document.querySelectorAll(selector));
+  result.forEach(addHoverFaceCheck);
+  // Create observer instance.
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      console.log("got mutation", mutation);
+      [].forEach.call(mutation.addedNodes, function(node) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          if(node.matches("#cfToolTip") || node.matches("#FaceCheck")) {
+            return;
+          }
+          var res = Array.prototype.slice.call(document.querySelectorAll(selector));
+          res.forEach(addHoverFaceCheck);
+        }
+      });
+    });
+  });
+
+  // Set up observer.
+  observer.observe(document, { childList: true, subtree: true });
+
+  return result;
+}
+
+
+$(document).ready(() => {
+  observeAllLinks()
+});
