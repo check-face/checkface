62b2e44cc77e1d3e29e625fb10ad219da266bd5b
apcteo <anapcteo@gmail.com>
2019-05-05 03:50:27 +1000

    Change querySelector order

--- src/extension/static/js/tags.js
+++ src/extension/static/js/tags.js
@@ -1,10 +1,9 @@
-var tags = document.querySelectorAll("a[href^='\/commit'], a[class*='sha']");
+var tags = document.querySelectorAll("a[class*='sha'],a[href^='\/commit\/']");
 var i;
 for(i = 0; i < tags.length; i++)
 {
     tags[i].onmouseover = function(){
       alert(tags.length);
-
     }
 
 }
