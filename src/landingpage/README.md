aff6d1010fad3262530cc4080010a3741a80e8a4
winahana <s3575233@student.rmit.edu.au>
2019-05-05 08:26:52 +1000

    Add Landing page

---
+++ src/landingpage/README.md
@@ -0,0 +1,33 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf810
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fnil\fcharset0 Menlo-Bold;}
+{\colortbl;\red255\green255\blue255;\red198\green124\blue72;\red0\green61\blue204;\red0\green68\blue254;
+\red255\green39\blue18;\red0\green68\blue254;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\csgenericrgb\c77800\c48800\c28400;\csgenericrgb\c0\c23922\c80000;\csgenericrgb\c0\c26667\c99608;
+\csgenericrgb\c100000\c15294\c7059;\csgenericrgb\c0\c26667\c99608;\csgenericrgb\c100000\c100000\c100000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww14140\viewh14260\viewkind0
+\deftab543
+\pard\tx543\pardeftab543\pardirnatural\partightenfactor0
+
+\f0\fs28 \cf2 \CocoaLigature0 \
+# \'93Nova\'94 Theme Landing Page (HTML)\
+\cf3 \
+Nova is a responsive HTML template, built on top of Bootstrap 4. \
+\
+\cf4 It is a freebie by\cf3  
+\f1\b \cf5 Webscope \cf4 -\cf5  
+\f0\b0 \cf6 https://webscopeapp.com : \
+\
+A  super easy and cool Client feedback tool. \cf7 \
+\cf2 \
+## License\
+\cf4 \
+Use it freely but please do not republish, distribute or sell "as-is". Please credit the designer and developer when you use it in your project.\cf3 \
+\cf2 \
+## Misc\cf7 \
+\
+\cf4 Check Out- https://webscopeapp.com\
+\
+Follow us on Twitter: {\field{\*\fldinst{HYPERLINK "https://twitter.com/webscopeapp"}}{\fldrslt https://twitter.com/webscopeapp}}\
+\
+Follow us on Facebook: {\field{\*\fldinst{HYPERLINK "https://www.facebook.com/webscopeapp/"}}{\fldrslt https://www.facebook.com/webscopeapp/}}\
+}
---
+++ src/landingpage/assets/js/script.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+
+    var myVideo = document.getElementById("video1");
+
+    function playPause() {
+        if (myVideo.paused)
+            myVideo.play();
+        else
+            myVideo.pause();
+    }
+
+    function makeBig() {
+        myVideo.width = 560;
+    }
+
+    function makeSmall() {
+        myVideo.width = 320;
+    }
+
+    function makeNormal() {
+        myVideo.width = 420;
+    }
+
+    function myFunction(event) {
+      event.preventDefault();
+      alert('this');
+
+      return true;
+    }
+
+
+    //Mouse click scroll
+    $(document).ready(function () {
+        $(".mouse").click(function () {
+            $('html, body').animate({scrollTop: '+=750px'}, 1200);
+        });
+    });
+
+    //Features appearance
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        //>=, not <=
+        if (scroll >= 500) {
+            $(".feature-icon").addClass("feature-display");
+            $(".feature-head-text").addClass("feature-display");
+            $(".feature-subtext").addClass("feature-display");
+        }
+    });
+
+    //Subscribe to newsletter
+    $('#object').on('submit', function (event) {
+      alert("wdldf");
+      event.preventDefault();
+        const form = event.target;
+        const data = new FormData(form);
+
+        var myImage = new Image();
+        myImage.src = 'checkface.ml/api/'+data.object.value;
+
+
+        document.getElementById("imagecontainer").appendChild(myImage);
+
+        return true;
+        });
+
+
+//smooth scrolling
+
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+
+})
