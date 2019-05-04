059dbddd7c9c1a0810115b39d76f4b4192e7d7a4
apcteo <anapcteo@gmail.com>
2019-05-05 01:06:21 +1000

    Get hash of download's id through the chrome extension (#14)

--- src/extension/background.js
+++ src/extension/background.js
@@ -5,13 +5,30 @@ var contextMenuItem={
   "contexts": ["selection"]
 };
 
+var fileIdHash = "";
+
+chrome.downloads.onCreated.addListener(function(downloadItem){
+var fileIdHash = new String(downloadItem.id).hashCode();
+//chrome.downloads.search(downloadItem.id, function()) - use if searching for a specific file that has already been downloaded is required
+});
+
 chrome.contextMenus.create(contextMenuItem);
 // Called when the user clicks on the browser action.
-chrome.contextMenus.onClicked.addListener(function(tab) {
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
   // Send a message to the active tab
-  console.log("hello");
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    chrome.tabs.sendMessage(activeTab.id, {"message": info.selectedText});
   });
 });
+
+String.prototype.hashCode = function() {
+var hash = 0, i, chr;
+if (this.length === 0) return hash;
+for (i = 0; i < this.length; i++) {
+  chr   = this.charCodeAt(i);
+  hash  = ((hash << 5) - hash) + chr;
+  hash |= 0; // Convert to 32bit integer
+}
+return hash;
+};
