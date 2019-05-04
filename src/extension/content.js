7a421ac6a4dbb998a9d63dfac522cba84203f3b8
apcteo <anapcteo@gmail.com>
2019-05-04 18:48:58 +1000

    Add dom in content.js, remove browser action from manifest.json

--- src/extension/content.js
+++ src/extension/content.js
@@ -9,32 +9,38 @@ chrome.runtime.onMessage.addListener(
         document.body.style.width = '100%';
 
         var div = document.createElement( 'div' );
-        var btnForm = document.createElement( 'form' );
-        var btn = document.createElement( 'input' );
+        var head = document.createElement( 'head');
+        var link = document.createElement('link');
+        var div_below = document.createElement( 'div' );
+        var p = document.createElement('p');
+        var img = document.createElement('img');
+        // var cssId = 'myCss';  // you could encode the css path itself to generate id..
+        // if (!document.getElementById(cssId))
+        // {
+        //     var head  = document.getElementsByTagName('head')[0];
+        //     var link  = document.createElement('link');
+        //     link.id   = cssId;
+        //     link.rel  = 'stylesheet';
+        //     link.type = 'text/css';
+        //     link.href = '../shared/styles.css';
+        //     link.media = 'all';
+        //     head.appendChild(link);
+        // }
 
-        //append all elements
         document.body.appendChild( div );
-        div.appendChild( btnForm );
-        btnForm.appendChild( btn );
-        //set attributes for div
-        div.id = 'myDivId';
-        div.style.position = 'fixed';
-        div.style.top = '50%';
-        div.style.left = '50%';
-        div.style.width = '100%';
-        div.style.height = '100%';
-        div.style.backgroundColor = 'none';
 
-        //set attributes for btnForm
-        btnForm.action = '';
+        div.class = 'container dark-theme';
+        div.style.position = 'absolute';
+        div.style.display = 'block';
+        div.style.zIndex = '1000';
+        div.style.width = '50%';
+        div.style.border = '5px solid red';
+        div.style.backgroundColor = 'white';
+        div.appendChild( div_below ).class = 'checkface';
+        div_below.style.zIndex = '1000';
+        div_below.appendChild(p).innerHTML = 'sasfdfshdllfdaflafdsja';
+        div_below.appendChild( img ).src = 'https://picsum.photos/300/300';
 
-        //set attributes for btn
-        //"btn.removeAttribute( 'style' );
-        btn.type = 'button';
-        btn.value = 'hello';
-        btn.style.position = 'absolute';
-        btn.style.top = '50%';
-        btn.style.left = '50%';
   }
 
   $(document).ready(myfunc);
