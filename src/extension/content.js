4ca623b1ce56cdfd729b2bc90c6850290f3f499e
apcteo <anapcteo@gmail.com>
2019-05-04 23:23:07 +1000

    Add request.message variable (highlighted text to pass)

--- src/extension/content.js
+++ src/extension/content.js
@@ -3,6 +3,7 @@ chrome.runtime.onMessage.addListener(
 
   function(request, sender, sendResponse) {
       let div;
+      let hashMessage = request.message;
       function myfunc(){
 
         div = document.createElement( 'div' );
@@ -20,7 +21,7 @@ chrome.runtime.onMessage.addListener(
         div_below.style.margin = "auto";
         div.style.zIndex = '99999999999';
         div_below.style.width = '500px';
-        div_below.style.height = '5000px';
+        div_below.style.height = '500px';
         div_below.style.border = '2px solid black';
         div_below.style.backgroundColor = 'white';
         div_below.appendChild( img ).src = 'https://picsum.photos/300/300';
