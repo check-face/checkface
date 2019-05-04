276bb532bf039d58067f80308e9780c867f998a6
Oliver Coad <oliver.coad@gmail.com>
2019-05-04 19:02:10 +0000

    Revert "Revert "Merge branch 'master' of https://gitlab.com/olivercoad/checkface""
    This reverts commit 1ee620e7d5036e2152fafb33c49b0d1df90251c7

--- src/extension/content.js
+++ src/extension/content.js
@@ -1,43 +1,77 @@
 // content.js
-chrome.runtime.onMessage.addListener(
 
-  function(request, sender, sendResponse) {
-      let div;
-      let hashMessage = request.message;
-      function myfunc(){
+function showPopup(hashMessage) {
+  let div;
+  hashMessage = hashMessage.trim();
+  console.log("Face Check:", hashMessage)
+  function myfunc() {
+    div = document.createElement('div');
+    div.innerHTML = `
+      <div id="FaceCheck">
+        <style>
+          #FaceCheck {
+            position: fixed;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            z-index: 9999999999999;
+            display: grid;
+            place-items: center;
+          }
 
-        div = document.createElement( 'div' );
-        div_below = document.createElement('div');
-        var img = document.createElement('img');
+          #FaceCheck .cfcontainer {
+            --text-color-normal: #333333;
+            --text-color-highlight: #D65A31;
+            --text-color-subtle: #999999;
+            --bg-color-main: #ECECEC;
+            --bg-color-alt: #CCCCCC;
+          }
+          
+          #FaceCheck.cfdark-theme {
+            --text-color-normal: #C4C6C3;
+            --text-color-highlight: #D65A31;
+            --text-color-subtle: #4B5365;
+            --bg-color-main: #222831;
+            --bg-color-alt: #21242B;
+          }
+          
+          #FaceCheck @media (prefers-color-scheme: dark) {
+            .container {
+            --text-color-normal: #C4C6C3;
+            --text-color-highlight: #D65A31;
+            --text-color-subtle: #4B5365;
+            --bg-color-main: #222831;
+            --bg-color-alt: #21242B;
+            }
+          }
+          
+          #FaceCheck .checkface {
+            background-color: var(--bg-color-main);
+            color: var(--text-color-normal);
+            padding: 10px;
+            border: 2px solid var(--text-color-subtle);
+          }
 
-        document.body.appendChild( div );
-        div.appendChild( div_below );
-        div.onclick = removeFunc;
-        div.style.position = 'sticky';
-        div.style.left = '0';
-        div.style.top = '0';
-        div.style.bottom = '0';
-        div.style.right = '0';
-        div_below.style.margin = "auto";
-        div.style.zIndex = '99999999999';
-        div_below.style.width = '500px';
-        div_below.style.height = '500px';
-        div_below.style.border = '2px solid black';
-        div_below.style.backgroundColor = 'white';
-        div_below.appendChild( img ).src = 'https://picsum.photos/300/300';
-        img.style.position = 'absolute';
-        img.style.left = '0';
-        img.style.top = '0';
-        img.style.bottom = '0';
-        img.style.right = '0';
-        img.style.margin = "auto";
-      }
+        </style>
+        <div class="cfcontainer">
+          <div class="checkface">
+            <p>sasfdfshdllfdaflafdsja</p>
+            <img id="checkfaceimg" width=300 height=300 />
+          </div>
+        </div>      
+      </div>
+    `
 
-      function removeFunc(){
-        div.remove();
-        return true;
-      }
-      $(document).ready(myfunc);
+    document.body.appendChild(div);
+    div.onclick = removeFunc;
+    document.querySelector("#checkfaceimg").src = "http://checkface.ml/api/" + hashMessage + "?dim=300";
+  }
 
-    }
-);
+  function removeFunc() {
+    div.remove();
+    return true;
+  }
+  $(document).ready(myfunc);
+}
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => showPopup(request.message));
