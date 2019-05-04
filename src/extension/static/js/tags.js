daa0d4eb952e05f826219e0f21c4a1e18559367c
Oliver Coad <oliver.coad@gmail.com>
2019-05-04 19:00:25 +0000

    Revert "Change querySelector order"
    This reverts commit 62b2e44cc77e1d3e29e625fb10ad219da266bd5b

--- src/extension/static/js/tags.js
+++ src/extension/static/js/tags.js
@@ -1,9 +1,10 @@
-var tags = document.querySelectorAll("a[class*='sha'],a[href^='\/commit\/']");
+var tags = document.querySelectorAll("a[href^='\/commit'], a[class*='sha']");
 var i;
 for(i = 0; i < tags.length; i++)
 {
     tags[i].onmouseover = function(){
       alert(tags.length);
+
     }
 
 }
