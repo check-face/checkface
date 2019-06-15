38ff5901c1785695f9368abec7d5e8712cf6f4b6
Oliver Coad <oliver.coad@gmail.com>
2019-06-16 09:53:55 +1000

    Target landingpage to local runway server

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,10 +1,51 @@
 $(document).ready(function () {
+    function getImage(seed) {
 
+        //generate normal distribution random number
+        function randn_bm() {
+            let u = 0, v = 0;
+            while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+            while(v === 0) v = Math.random();
+            return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+        }
+
+        function genVec(seed) {
+            Math.seedrandom(seed)
+            let arr = []
+            for(let i = 0; i < 512; i++) {
+                arr.push(randn_bm())
+            }
+            return arr
+        }
+
+        const inputs = {
+            "z": genVec(seed),
+            "truncation": 0.7
+        };
+
+        return fetch('http://localhost:8001/query', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(inputs)
+        })
+        .then(response => response.json())
+        .then(output => {
+            const { image } = output;
+            return image
+        })
+    }
     function myFunction() {
        
         var myImage = document.getElementById("my-image");
         var value = document.getElementById('object').value;
-        myImage.src = 'https://api.checkface.ml/api/' + value + "?dim=500";
+        // myImage.src = 'https://api.checkface.ml/api/' + value + "?dim=500";
+        getImage(value).then(image => {
+            myImage.src = image;
+        })
+
         window.history.replaceState({ value: value }, 'Check Face - ' + value, '?value=' + value);
         return false;
     }
@@ -13,7 +54,10 @@ $(document).ready(function () {
     const currentVal = urlParams.get('value');
     if(currentVal) {
         document.getElementById('object').value = currentVal;
-        document.getElementById("my-image").src = 'https://api.checkface.ml/api/' + currentVal + "?dim=500";
+        // document.getElementById("my-image").src = 'https://api.checkface.ml/api/' + currentVal + "?dim=500";
+        getImage(currentVal).then(image => {
+            document.getElementById("my-image").src = image;
+        })
     }
 
     //Mouse click scroll
